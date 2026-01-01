81e2329692d8dc1d2ccd7f348d405a0998ac3ada
Harsh Kanjiya <harshkanjiya001@gmail.com>
2026-01-02 00:41:53 +0530

    feat: simplify Page component to redirect to external URL

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,6 @@
-import CTASection from '@/components/CTASectionb'
-import FeaturesSection from '@/components/FeaturesSection'
-import Footer from '@/components/Footer'
-import HeroSection from '@/components/HeroSection'
-import Navbar from '@/components/Navbar'
-import PlaygroundSection from '@/components/PlaygroundSection'
-
+import { redirect } from 'next/navigation';
 const Page = () => {
-  return (
-    <div className="min-h-screen bg-background flex flex-col items-center justify-center mx-auto">
-      <Navbar />
-      <main>
-        <HeroSection />
-        <FeaturesSection />
-        <PlaygroundSection />
-        <CTASection />
-      </main>
-      <Footer />
-    </div>
-  )
+  redirect('https://escform.com');
 }
 
 export default Page
